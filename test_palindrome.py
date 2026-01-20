5c36472f28626a4733cbe8c4f0a2f077c5044339
Issy Smith <IssySmith@ibm.com>
2026-01-20 13:54:39 +0000

    more tests

--- test_palindrome.py
+++ test_palindrome.py
@@ -2,6 +2,8 @@ from main import is_palindrome
 
 def test_is_palindrome_true():
     assert is_palindrome(121) == True
+    assert is_palindrome(12121) == True
     
 def test_is_palindrome_false():
     assert is_palindrome(122) == False
+    assert is_palindrome(12122) == False
