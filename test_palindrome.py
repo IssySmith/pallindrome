5083f71975964d2522d0128059c5b95896672cbe
Issy Smith <IssySmith@ibm.com>
2026-01-20 14:05:40 +0000

    whitespace test

--- test_palindrome.py
+++ test_palindrome.py
@@ -5,6 +5,7 @@ def test_is_palindrome_true():
     assert is_palindrome(12121) == True
     assert is_palindrome("hannah") == True
     assert is_palindrome("Hannah") == True
+    assert is_palindrome("race car") == True
     
     
 def test_is_palindrome_false():
@@ -12,3 +13,4 @@ def test_is_palindrome_false():
     assert is_palindrome(12122) == False
     assert is_palindrome("steve") == False
     assert is_palindrome("Steve") == False
+    assert is_palindrome("race cart") == False
